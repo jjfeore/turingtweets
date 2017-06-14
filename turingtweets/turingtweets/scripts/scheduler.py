479f590aea656a9ddbc88538ee05709cc400fc20
Miguel Pena <miguelp1986@gmail.com>
2017-06-14 12:24:43 -0700

    trying apscheduler with heroku

--- turingtweets/turingtweets/scripts/scheduler.py
+++ turingtweets/turingtweets/scripts/scheduler.py
@@ -1,20 +1,10 @@
-# import schedule
-# import tweepy
-# import time
-# import os
-# from turingtweets.scripts.tweet_fake_tweet import get_fake_tweet, tweet_fake_tweet
-# from turingtweets.scripts.update_tweet_db import <FUNCTIONS TO IMPORT>
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    print('This job is run every three minutes.')
 
 
-############### SCHEDULE TEMPLATE ##############
-# def job():
-#     print("I'm working...")
-#
-# schedule.every().hour.do(job)
-# schedule.every().day.at("10:30").do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
-#
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+sched.start()
