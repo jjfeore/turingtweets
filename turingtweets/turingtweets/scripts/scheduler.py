376a00922b213384af8114bdfdb6cf40160a0514
Miguel Pena <miguelp1986@gmail.com>
2017-06-14 12:51:22 -0700

    trying scheduling stuff now

--- turingtweets/turingtweets/scripts/scheduler.py
+++ turingtweets/turingtweets/scripts/scheduler.py
@@ -1,10 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from turingtweets.scripts.tweet_fake_tweet import get_fake_tweet, tweet_fake_tweet
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every minute.')
 
 
 sched.start()
