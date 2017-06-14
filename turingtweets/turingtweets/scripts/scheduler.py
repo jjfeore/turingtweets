12181ba33e0e5494a9b9fec1e334bdea451c0f33
Miguel Pena <miguelp1986@gmail.com>
2017-06-14 13:11:30 -0700

    trying scheduling stuff now

--- turingtweets/turingtweets/scripts/scheduler.py
+++ turingtweets/turingtweets/scripts/scheduler.py
@@ -1,12 +1,12 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from turingtweets.scripts.tweet_fake_tweet import get_fake_tweet, tweet_fake_tweet
-import logging
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    logging.exception('THIS JOB IS RUN EVERY MINUTE.')
-
-
-sched.start()
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# from turingtweets.scripts.tweet_fake_tweet import get_fake_tweet, tweet_fake_tweet
+# import logging
+#
+# sched = BlockingScheduler()
+#
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
+#     logging.exception('THIS JOB IS RUN EVERY MINUTE.')
+#
+#
+# sched.start()
